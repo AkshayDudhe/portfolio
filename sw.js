81c778e93e1d87d00262d888284cb2541aa6c5af
Akshay dudhe <59861660+AkshayDudhe@users.noreply.github.com>
2025-01-03 18:38:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,3 +9,24 @@ self.addEventListener('push', (event) => {
 
   self.registration.showNotification(data.title || 'Hello world!', options);
 });
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close(); // Close the notification when clicked
+
+  const redirectUrl = event.notification.data.url; // Get the URL from the notification's data
+
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then((clientList) => {
+      // Check if the URL is already open in a tab
+      for (let client of clientList) {
+        if (client.url === redirectUrl && 'focus' in client) {
+          return client.focus(); // Focus the existing tab with the same URL
+        }
+      }
+
+      // If not, open the URL in a new tab
+      if (clients.openWindow) {
+        return clients.openWindow(redirectUrl); // Open the URL in a new tab
+      }
+    })
+  );
+});
